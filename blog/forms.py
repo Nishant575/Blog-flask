03261551a4c4b6d97f7d301b4eef787420068bf9
Nishant Dalvi <61659609+Nishant575@users.noreply.github.com>
2021-05-20 02:56:07 +0530

    third commit

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm  
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -10,12 +12,12 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
 
-    bio = TextAreaField('Bio')
-
     password = PasswordField('Password', validators=[DataRequired()])
     
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])  
+
+
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -36,3 +38,33 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+
+    picture = FileField('Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already exists')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exists')    
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    picture = FileField('Blog picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Post')
+    
+
+
