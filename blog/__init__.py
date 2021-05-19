5d7159680286f8e3495be9b6f0d7c9bd3746e6ec
Nishant Dalvi <61659609+Nishant575@users.noreply.github.com>
2021-05-19 16:22:59 +0530

    second commit

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '41c308f9f8d93d135dc81006f0e4363f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from blog import routes
